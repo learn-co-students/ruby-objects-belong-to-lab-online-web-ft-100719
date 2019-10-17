b45c0cf94afdb8609c5cdb0140f05d7d06f4325e
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-17 14:47:04 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,9 @@
 class Artist
   attr_accessor :name
+@@song_count = 0
 
-  def initialize(name)
-    @name = name
+  def initialize
+    @name= name
+    @songs= []
   end
 end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+  attr_accessor :song, :artist, :title
+
+  def initialize
+    @song = song
+    @artist = artist
+    @title = title
+  end
+end
