7eaed800c131e11d490a58095726a507c0b68e09
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-21 16:29:10 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,4 @@
-class Song
-attr_accessor :title, :artist
-
-end
-
-
 class Artist
 attr_accessor :name
   
-end
-
-
-class Post
-attr_accessor :title, :author
-
-end
-
-
-class Author
-attr_accessor :name
-
 end
