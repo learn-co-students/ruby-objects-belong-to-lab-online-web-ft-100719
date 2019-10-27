709451c8e84587f01207975aba84a6bea5964320
Prince Charles <princecharles35@yahoo.com>
2019-10-27 17:26:53 +0000

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,9 +1,32 @@
-require "spec_helper"
+class Artist
+  attr_accessor :name
 
-describe "Artist" do 
-  it "has a name" do
-    artist = Artist.new
-    artist.name = "Beyonce" 
-    expect(artist.name).to eq("Beyonce")
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
+  end
+
+  def self.song_count
+    @@song_count
   end
 end
+
